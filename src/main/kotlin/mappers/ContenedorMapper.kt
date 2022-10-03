d4b7dce2c7b9e55b5f2154d6110e520075cf8b3a
Mireya Sánchez Pinzón <mireyasanchz02@gmail.com>
2022-10-03 20:52:50 +0200

    Implementación Companion Object clases Enum y creación ContenedorMapper.

--- src/main/kotlin/mappers/ContenedorMapper.kt
+++ src/main/kotlin/mappers/ContenedorMapper.kt
@@ -1,5 +1,49 @@
 package mappers
 
-class ContenedorMapper {
+import dto.ContenedorDTO
+import models.Contenedor
+import models.TipoContenedor
+import models.TipoVia
 
+fun ContenedorDTO.toContenedor(): Contenedor {
+    return Contenedor(
+        codigo = this.codigo,
+        contenedor = TipoContenedor.from(this.contenedor),
+        modelo = this.modelo,
+        descripcion = this.descripcion,
+        cantidad = this.cantidad,
+        lote = this.lote,
+        distrito = this.distrito,
+        barrio = this.barrio,
+        via = TipoVia.from(this.via),
+        nombre = this.nombre,
+        numero = this.numero,
+        coordenadaX = this.coordenadaX,
+        coordenadaY = this.coordenadaY,
+        longitud = this.longitud,
+        latitud = this.latitud,
+        direccion = this.direccion,
+    )
+}
+
+fun Contenedor.toDTO(): ContenedorDTO {
+    return ContenedorDTO(
+        codigo = this.codigo,
+        contenedor = this.contenedor.toString(),
+        modelo = this.modelo,
+        descripcion = this.descripcion,
+        cantidad = this.cantidad,
+        lote = this.lote,
+        distrito = this.distrito,
+        barrio = this.barrio,
+        via =this.via.toString(),
+        nombre = this.nombre,
+        numero = this.numero,
+        coordenadaX = this.coordenadaX,
+        coordenadaY = this.coordenadaY,
+        longitud = this.longitud,
+        latitud = this.latitud,
+        direccion = this.direccion,
+
+    )
 }
--- src/main/kotlin/mappers/ResiduoMapper.kt
+++ src/main/kotlin/mappers/ResiduoMapper.kt
@@ -1,4 +1,5 @@
 package mappers
 
 class ResiduoMapper {
+
 }
